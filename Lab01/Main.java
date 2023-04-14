11e55b0f5c635e2e7b8f0235e7934b8d068a0fe6
congduytran12 <109121562+congduytran12@users.noreply.github.com>
2023-04-14 14:12:55 +0700

    Add files via upload

---
+++ Lab01/Main.java
@@ -0,0 +1,9 @@
+
+// Online IDE - Code Editor, Compiler, Interpreter
+
+public class Main
+{
+    public static void main(String[] args) {
+        System.out.println("Welcome to Online IDE!! Happy Coding :)");
+    }
+}
