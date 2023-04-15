49e4524ea68cece84c3a5e784c299e350e5c56a2
Tran Cong Duy <109121562+congduytran12@users.noreply.github.com>
2023-04-15 16:43:19 +0700

    Add files via upload

---
+++ Lab01/ArraySort.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class ArraySort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter the number of elements: ");
+        int n = sc.nextInt();
+        System.out.println("Please enter the input array: ");
+        int[] a = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        for(int i = 0; i < n; i++) {
+            for(int j = i + 1; j < n; j++) {
+                if (a[i] > a[j]) {
+                    int temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
+            }
+        }
+
+        System.out.println("Sorted array: ");
+        for(int i = 0; i < n; i++) {
+            System.out.print(a[i] + " ");
+        }
+        System.out.println();
+
+        int sumOfArray = 0;
+        for (int i = 0; i < n; i++) {
+            sumOfArray += a[i];
+        }
+        System.out.println("Sum of array elements: " + sumOfArray);
+        System.out.println("Average value of array elements: " + (sumOfArray / n));
+    }
+}
