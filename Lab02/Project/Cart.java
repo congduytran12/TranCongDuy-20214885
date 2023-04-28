7b577799ccc83f987c39a21bb5c04fd9cff26750
Tran Cong Duy <109121562+congduytran12@users.noreply.github.com>
2023-04-28 14:05:25 +0700

    Add files via upload

---
+++ Lab02/Project/Cart.java
@@ -0,0 +1,16 @@
+public class Cart {
+    public static final int MAX_NUMBERS_ORDERED = 20;
+    private DigitalVideoDisc itemsOrdered[] = new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
+
+    public void addDigitalVideoDisc(DigitalVideoDisc disc) {
+
+    }
+
+    public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
+
+    }
+
+    public float totalCost() {
+
+    }
+}
