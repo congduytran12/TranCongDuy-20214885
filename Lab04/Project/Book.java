5d3ada6ebf13079305f7a37b8d1d37755b4d4121
Tran Cong Duy <109121562+congduytran12@users.noreply.github.com>
2023-05-26 13:16:38 +0700

    Update Book.java

--- Lab04/Project/Book.java
+++ Lab04/Project/Book.java
@@ -1,40 +1,11 @@
 import java.util.ArrayList;
 
-public class Book {
-    private int id;
-    private String title;
-    private String category;
+public class Book extends Media {
     private ArrayList<String> authors;
     
     public Book(int id, String title, String category, ArrayList<String> authors) {
-        this.id = id;
-        this.title = title;
-        this.category = category;
+        super(id, title, category);
         this.authors = authors;
-    }
-    
-    public int getId() {
-        return id;
-    }
-    
-    public void setId(int id) {
-        this.id = id;
-    }
-    
-    public String getTitle() {
-        return title;
-    }
-    
-    public void setTitle(String title) {
-        this.title = title;
-    }
-    
-    public String getCategory() {
-        return category;
-    }
-    
-    public void setCategory(String category) {
-        this.category = category;
     }
     
     public ArrayList<String> getAuthors() {
