57013249979db95792c61de0d5ae3653496f8779
Tran Cong Duy <109121562+congduytran12@users.noreply.github.com>
2023-05-26 13:52:31 +0700

    Update CompactDisc.java

--- Lab04/Project/CompactDisc.java
+++ Lab04/Project/CompactDisc.java
@@ -1,5 +1,40 @@
+import java.util.ArrayList;
+
 public class CompactDisc extends Disc {
-    public CompactDisc(int id, String title, String category, float cost, int length, String director) {
+    private String artist;
+    private ArrayList<Track> tracks;
+    
+    public CompactDisc(int id, String title, String category, float cost, int length, String director, String artist, ArrayList<Track> tracks) {
         super(id, title, category, cost, length, director);
+        this.artist = artist;
+        this.tracks = tracks;
+    }
+    
+    public String getArtist() {
+        return artist;
+    }
+    
+    public void addTrack(Track track) {
+        if (tracks.contains(track)) {
+            System.out.println("The track is already in the list");
+        } else {
+            tracks.add(track);
+        }
+    }
+    
+    public void removeTrack(Track track) {
+        if (tracks.contains(track)) {
+            tracks.remove(track);
+        } else {
+            System.out.println("The track is not existed in the list");
+        }
+    }
+    
+    public int getLength() {
+        int length = 0;
+        for (Track track : tracks) {
+            length += track.getLength();
+        }
+        return length;
     }
 }
