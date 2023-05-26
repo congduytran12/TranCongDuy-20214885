d68176a935de88864f2ef6d0dadd7405201b84f6
Tran Cong Duy <109121562+congduytran12@users.noreply.github.com>
2023-05-26 13:34:44 +0700

    Add files via upload

---
+++ Lab04/Project/CompactDisc.java
@@ -0,0 +1,5 @@
+public class CompactDisc extends Disc {
+    public CompactDisc(int id, String title, String category, float cost, int length, String director) {
+        super(id, title, category, cost, length, director);
+    }
+}
