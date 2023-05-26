33b76d8445d671a9fc2cf00cfe6bcadef02f907a
Tran Cong Duy <109121562+congduytran12@users.noreply.github.com>
2023-05-26 14:03:46 +0700

    Update CompactDisc.java

--- Lab04/Project/CompactDisc.java
+++ Lab04/Project/CompactDisc.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 
-public class CompactDisc extends Disc {
+public class CompactDisc extends Disc implements Playable{
     private String artist;
     private ArrayList<Track> tracks;
     
