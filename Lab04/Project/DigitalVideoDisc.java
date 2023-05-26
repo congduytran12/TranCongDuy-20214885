99e99cc436d0919f6e79044d6fdda09bf43663e9
Tran Cong Duy <109121562+congduytran12@users.noreply.github.com>
2023-05-26 13:19:52 +0700

    Update DigitalVideoDisc.java

--- Lab04/Project/DigitalVideoDisc.java
+++ Lab04/Project/DigitalVideoDisc.java
@@ -1,42 +1,12 @@
-public class DigitalVideoDisc {
-    private String title;
-    private String category;
+public class DigitalVideoDisc extends Media{
     private String director;
-    private int length;
-    private float cost;
-    private int id = 0;
+    private int length;       
     private static int nbDigitalVideoDiscs = 0;
 
-    public DigitalVideoDisc(String title) {
-        this.title = title;
-        id++;
-    }
-
-    public DigitalVideoDisc(String title, String category, float cost) {
-        this(title);
-        this.category = category;
-        this.cost = cost;
-        id++;
-    }
-
-    public DigitalVideoDisc(String title, String category, String director, float cost) {
-        this(title, category, cost);
-        this.director = director;
-        id++;
-    }
-
     public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
-        this(title, category, director, cost);
+        super(title, category, cost);
         this.length = length;
-        id++;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getCategory() {
-        return category;
+        this.cost = cost;
     }
 
     public String getDirector() {
@@ -47,32 +17,12 @@ public class DigitalVideoDisc {
         return length;
     }
 
-    public float getCost() {
-        return cost;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
     public void setDirector(String director) {
         this.director = director;
     }
 
     public void setLength(int length) {
         this.length = length;
-    }
-
-    public void setCost(float cost) {
-        this.cost = cost;
     }
 
     public boolean isMatch(String title) {
